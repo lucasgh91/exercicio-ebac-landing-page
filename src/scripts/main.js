14e9e0eca80a7c0289fba0bfa60dfc8d11d2d712
Lucas G. <ghizzolucas@gmail.com>
2024-10-18 03:10:20 -0300

    Criado: .faq

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
-
-
 window.addEventListener('DOMContentLoaded', function() {
+  const questions = document.querySelectorAll('[data-faq-question]')
   const tabButtons = document.querySelectorAll('[data-tab-button]')
 
   for (let i = 0; i < tabButtons.length; i++) {
@@ -12,6 +11,13 @@ window.addEventListener('DOMContentLoaded', function() {
       aba.classList.add('wallpapers__list--is-active')
       e.target.classList.add('wallpapers__tabs__button--is-active')
     })
+  }
+
+  for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('click', function(e) {
+      e.target.parentNode.classList.toggle('faq__questions__item--is-open')
+    })
+    
   }
 })
 
