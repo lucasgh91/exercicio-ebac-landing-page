773b87ec028cee9581144cc1fd666fbdeeaa5d1c
Lucas G. <ghizzolucas@gmail.com>
2024-10-19 01:13:02 -0300

    Criado arquivos configs.scss e variaveis.scss. Ajustada responsividade.

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
 
 function styles() {
-  return gulp.src('./src/styles/*.scss')
+  return gulp.src('./src/styles/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'))
 }
